ac97b6630368e544a8d04540ecd40d5ff78c34bc
Sayuri <sayurikamble123@gmail.com>
2022-08-10 19:56:27 +0530

    init create-react-library@3.1.1

---
+++ README.md
@@ -0,0 +1,30 @@
+# custom-cursor-sayuk
+
+> 
+
+[![NPM](https://img.shields.io/npm/v/custom-cursor-sayuk.svg)](https://www.npmjs.com/package/custom-cursor-sayuk) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Install
+
+```bash
+npm install --save custom-cursor-sayuk
+```
+
+## Usage
+
+```jsx
+import React, { Component } from 'react'
+
+import MyComponent from 'custom-cursor-sayuk'
+import 'custom-cursor-sayuk/dist/index.css'
+
+class Example extends Component {
+  render() {
+    return <MyComponent />
+  }
+}
+```
+
+## License
+
+MIT © [SAYUK09](https://github.com/SAYUK09)
---
+++ src/components/CustomCursor.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import "./customCursor.css"
+import { useEffect, useState } from "react";
+
+const CustomCursor=() =>{
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [animation, setAnimation] = useState(false);
+  console.log(position,"psss")
+
+  useEffect(() => {
+    document.addEventListener("mousemove", (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY });
+    });
+  }, []);
+
+  useEffect(() => {
+    const mouseClickHandler = () => {
+      setAnimation(true);
+      const timeOutId = setTimeout(() => {
+        setAnimation(false);
+      }, 100);
+      return () => clearTimeout(timeOutId);
+    };
+    document.addEventListener("click", mouseClickHandler);
+
+    return () => {
+      document.removeEventListener("click", mouseClickHandler);
+    };
+  }, []);
+
+  return (
+    <>
+      <div
+        className={`cursor ring ${animation ? "animate" : ""}`}
+        style={{ left: position.x, top: position.y }}
+      />
+      <div
+        className="cursor dot"
+        style={{ left: position.x, top: position.y }}
+      />
+    </>
+  );
+}
+
+
+export default CustomCursor;
