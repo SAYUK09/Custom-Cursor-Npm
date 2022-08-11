49c71f6dd608cb4c881ffce53b048b3e106e1ca6
Sayuri <sayurikamble123@gmail.com>
2022-08-11 11:44:07 +0530

    custom cursor published

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
-import { ExampleComponent } from 'custom-cursor-sayuk'
+import { CustomCursor } from 'customized-cursor'
 import 'custom-cursor-sayuk/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <div>
+      <CustomCursor />
+    </div>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,47 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { useEffect, useState } from 'react'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const CustomCursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [animation, setAnimation] = useState(false)
+
+  useEffect(() => {
+    document.addEventListener('mousemove', (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    })
+  }, [])
+
+  useEffect(() => {
+    const mouseClickHandler = () => {
+      setAnimation(true)
+      const timeOutId = setTimeout(() => {
+        setAnimation(false)
+      }, 100)
+      return () => clearTimeout(timeOutId)
+    }
+    document.addEventListener('click', mouseClickHandler)
+
+    return () => {
+      document.removeEventListener('click', mouseClickHandler)
+    }
+  }, [])
+
+  return (
+    <div>
+      <div
+        className={`${styles.cursor} ${styles.ring} ${
+          animation ? styles.animate : styles.ring
+        } `}
+        style={{ left: position.x, top: position.y }}
+      />
+      <div
+        className={`${styles.cursor} ${styles.dot}`}
+        style={{ left: position.x, top: position.y }}
+      />
+    </div>
+  )
 }
+
+export default CustomCursor
